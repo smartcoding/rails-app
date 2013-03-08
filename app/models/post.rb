947877fb4c0f36ee5025771016873f10da8986c2
Sergey Lukin <contact@sergeylukin.com>
2013-03-08 02:45:06 +0200

    models-relations: make sure comments and likes are destroyed when post is destroyed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :timelines, :as => :timelineable
 
   is_impressionable :counter_cache => { :column_name => :views_count }
