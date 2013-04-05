c5e9bd089eb8be9e0212c530c1eba072e6818eb9
Sergey Lukin <contact@sergeylukin.com>
2013-04-05 14:30:23 +0300

    Revert "Add type column to Posts"
    This reverts commit 2e464204d15624092ddcfddb0e49622732f00792.

--- db/migrate/20130402210952_add_type_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddTypeToPosts < ActiveRecord::Migration
-  def change
-    # Colum names should be named like {{enum_name}}_cd
-    add_column :posts, :type_cd, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130402210952) do
+ActiveRecord::Schema.define(:version => 20130402185153) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(:version => 20130402210952) do
     t.integer  "likes_count",     :default => 0
     t.integer  "comments_count",  :default => 0
     t.text     "additional_body"
-    t.integer  "type_cd"
   end
 
   add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
